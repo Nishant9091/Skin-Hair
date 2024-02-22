5ef846a4868998b6d8bed2ffd2bfe1919198d40e
Nishant9091 <nishantsharma7428@gmail.com>
2024-02-22 18:48:32 +0530

    newpage

--- assets/js/YouTubePopUp.js
+++ assets/js/YouTubePopUp.js
@@ -39,3 +39,17 @@
       });
   };
 })(jQuery);
+
+
+// ytbpopup
+function openPopup(videoId) {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("youtube-video").src = "https://www.youtube.com/embed/" + videoId;
+}
+
+function closePopup() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("youtube-video").src = ""; // Reset the video source
+}
